952fe854fd628b3805ba42b7faa2b35e3df8e29e
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-04-11 03:17:52 +0100

    if else

---
+++ src/conditions/IfElseStatement.java
@@ -0,0 +1,10 @@
+package conditions;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        boolean isAuthenticated = true;
+        if (isAuthenticated)
+            System.out.println("You are logged In");
+            else System.out.println("Please Log In");
+    }
+}
