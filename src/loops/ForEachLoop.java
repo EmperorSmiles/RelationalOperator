0983ef961304582050de6e3414d8676f9f0f2bee
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-05-03 03:45:08 +0100

    More tests

--- src/loops/ForEachLoop.java
+++ src/loops/ForEachLoop.java
@@ -3,7 +3,7 @@ package loops;
 public class ForEachLoop {
     public static void main(String[] args) {
     // AKA Enhanced for Loop
-        String names[] ={"Tobi", "Michael", "San", "Kamsey"};
+        String[] names ={"Tobi", "Michael", "San", "Kamsey"};
 
         for (int i = 0; i < names.length; i++) {
             System.out.println(names[i]);
